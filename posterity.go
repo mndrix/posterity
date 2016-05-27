35992f9bb9d804573e88469c4b80df6a12affbf8
Michael Hendricks <michael@ndrix.org>
2016-05-27 12:28:22 -0600

    Simulate family assets
    The family starts with a certain endowment.  Each month, some of that
    income is given away to the youngest members of the family.  Show a
    histogram of how the family's assets turn out.

--- posterity.go
+++ posterity.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 type Family struct {
+	Assets    int     // family assets in dollars
+	Inflation float64 // inflation rate
+	Return    float64 // return on assets
+
 	People []*Person
 }
 
@@ -31,6 +35,27 @@ func (f *Family) Next() {
 		people = append(people, xs...)
 	}
 	f.People = people
+
+	// handle family assets
+	income := float64(f.Assets) * (f.Return - f.Inflation)
+	for _, x := range f.People {
+		if x.Age == 13 || x.Age == 14 {
+			income -= 1200 // $100 per month
+		}
+		if x.Age == 15 || x.Age == 16 {
+			income -= 2400 // $200 per month
+		}
+		if x.Age == 17 {
+			income -= 4800 // $400 per month
+		}
+		if x.Age >= 18 && x.Age <= 35 {
+			income -= 12000 // $1,000 per month
+		}
+		if income < 0 { // or oldest ones get no money
+			income = 0
+		}
+	}
+	f.Assets += int(income)
 }
 
 func main() {
@@ -40,9 +65,18 @@ func main() {
 	smallestSize := 999
 	largestSize := 0
 	sizes := make(map[int]int)
+
+	smallestValue := 999
+	largestValue := 0
+	values := make(map[int]int)
+
 	for i := 0; i < iterations; i++ {
 
 		family := &Family{
+			Assets:    1000000,
+			Inflation: 0.01,
+			Return:    0.05,
+
 			People: []*Person{
 				{Male, 37, 5},   // me
 				{Female, 12, 0}, // Ara
@@ -65,8 +99,23 @@ func main() {
 		if size < smallestSize {
 			smallestSize = size
 		}
+
+		value := family.Assets / 100000
+		values[value]++
+		if value > largestValue {
+			largestValue = value
+		}
+		if value < smallestValue {
+			smallestValue = value
+		}
 	}
 
+	fmt.Println("\nAssets:")
+	for i := smallestValue; i <= largestValue; i++ {
+		fmt.Printf("%.1f %.1f\n", float64(i)/10, float64(values[i])/float64(iterations)*100)
+	}
+
+	fmt.Println("\nFamily size:")
 	for i := smallestSize; i <= largestSize; i++ {
 		fmt.Printf("%2d %.1f\n", i, float64(sizes[i])/float64(iterations)*100)
 	}
